5f47ea571ee3ae4f1bb09088ddddc06039321335
alasaidi <alasaidi55gmail.com>
2024-05-30 18:44:27 +0200

    sorting data function done

--- src/handlers/sort-user-by-age.js
+++ src/handlers/sort-user-by-age.js
@@ -1,1 +1,17 @@
-// export const sortUserByAge = async () => {};
+export const sortUserByAge = () => {
+  let users = document.getElementsByClassName("user-profile");
+  const userArray = [...users];
+  //   const age = userArray[0].getAttribute("data-age");
+  console.log(userArray);
+  console.log(userArray);
+  const list = userArray.sort(
+    (a, b) => a.getAttribute("data-age") - b.getAttribute("data-age"),
+  );
+  //   console.log(list);
+  const root = document.getElementById("users");
+  //   root.innerHTML = "";
+  for (let div of list) {
+    console.log(div);
+    root.appendChild(div);
+  }
+};
